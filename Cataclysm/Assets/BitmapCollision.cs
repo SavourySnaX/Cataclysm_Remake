d3c01e738ae86d7ad9e96eacbf57d8fdf52c60a2
Savoury SnaX <savoury.snax@googlemail.com>
2017-10-14 21:34:49 +0100

    first part, can't get tilemap to work..

---
+++ Cataclysm/Assets/BitmapCollision.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class BitmapCollision : MonoBehaviour 
+{
+	// Use this for initialization
+	void Start () 
+	{
+		TileMap tm = transform.parent.GetComponent<TileMap>;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
