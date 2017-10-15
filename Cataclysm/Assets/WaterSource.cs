bed1712cee2cf6b0d8ee4ee7a58aece2f1d55d68
Savoury SnaX <savoury.snax@googlemail.com>
2017-10-15 17:50:32 +0100

    Working on automatic water pumps

--- Cataclysm/Assets/WaterSource.cs
+++ Cataclysm/Assets/WaterSource.cs
@@ -1,8 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class WaterSource : MonoBehaviour {
+
+	public Vector3[] spawnLocations;
+	public int numSpawnLocations;
 
 	public int totalDrops=500;
 	public float spawnTimer=0.25f;
@@ -12,12 +16,55 @@ public class WaterSource : MonoBehaviour {
 	public GameObject background;
 	BoxCollider2D bc;
 	int layerMaskForMovement;
+	Tilemap tm;
 
 	// Use this for initialization
 	void Start () 
 	{
 		bc = prefab.GetComponent<BoxCollider2D> ();
 		layerMaskForMovement = LayerMask.GetMask ("Background", "Interaction", "Water", "Player");
+
+		spawnLocations = new Vector3[20];
+		numSpawnLocations = 0;
+
+		tm = GetComponent<Tilemap>();
+		for (int x = 0; x < tm.cellBounds.size.x; x++) 
+		{
+			for (int y = 0; y < tm.cellBounds.size.y; y++) 
+			{
+				Sprite s = tm.GetSprite (new Vector3Int (x, y, 0));
+
+				if (s == null)
+					continue;
+
+				Debug.Log (x+" "+y+" "+s.name);
+
+				Vector3 spawnStart;
+				switch (s.name) 
+				{
+				case "Water_L":
+					spawnStart = new Vector3 (-1 / 3.0f, 0, 0);
+					break;
+				case "Water_R":
+					spawnStart = new Vector3 (4 / 3.0f, 0, 0);
+					break;
+				case "Water_U":
+					spawnStart = new Vector3 (0, 13 / 12.0f, 0);
+					break;
+				case "Water_D":
+					spawnStart = new Vector3 (0, -1 / 12.0f, 0);
+					break;
+				default:
+					continue;
+				}
+
+				var vec = tm.CellToWorld (new Vector3Int (x, y,0))+tm.origin;
+				vec += spawnStart;
+
+				spawnLocations [numSpawnLocations++] = vec;
+			}
+		}
+
 	}
 	
 	// Update is called once per frame
