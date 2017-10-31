c9fc36317ebe18e37d39a02336f3d34e583d2537
SavourySnaX <savoury.snax@googlemail.com>
2017-10-31 12:24:53 +0000

    Improved title screen

---
+++ Cataclysm/Assets/Scripts/MainMenuCamera.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuCamera : MonoBehaviour 
+{
+
+	public float speed;
+
+	public float leftExtent;
+	public float rightExtent;
+	public float topExtent;
+	public float bottomExtent;
+	public float finalScale;
+
+	float length;
+	float dir;
+
+	void Start () 
+	{
+		length = 0.0f;
+		dir = 1;
+
+		Camera.main.orthographicSize = Mathf.Lerp (6, finalScale, length);
+		transform.position=new Vector3( Mathf.Lerp(leftExtent,rightExtent,length) , Mathf.Lerp(bottomExtent,topExtent,length), transform.position.z);
+	}
+	
+	void FixedUpdate () 
+	{
+		length += dir * speed * Time.deltaTime;
+		if (dir > 0.0f)
+		{
+			if (length >= 1.0f)
+			{
+				dir = 0.0f;
+				length = 1.0f;
+			}
+		}
+		if (dir < 0.0f)
+		{
+			if (length <= 0.0f)
+			{
+				dir = 1.0f;
+				length = 0.0f;
+			}
+		}
+
+		Camera.main.orthographicSize = Mathf.Lerp (6, finalScale, length);
+		transform.position=new Vector3( Mathf.Lerp(leftExtent,rightExtent,length) , Mathf.Lerp(bottomExtent,topExtent,length), transform.position.z);
+	}
+}
