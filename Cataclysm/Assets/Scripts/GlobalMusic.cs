a6974222939dd53b0b6fd2d0d60414ba88f08d0e
SavourySnaX <savoury.snax@googlemail.com>
2017-11-02 10:58:08 +0000

    Added a music track

---
+++ Cataclysm/Assets/Scripts/GlobalMusic.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalMusic : MonoBehaviour 
+{
+	private static GlobalMusic instance = null;
+
+	void Awake()
+	{
+		if (instance == null)
+		{
+			instance = this;
+			DontDestroyOnLoad (this.gameObject);
+		} 
+		else
+		{
+			DestroyObject (this.gameObject);
+		}
+	}
+}
--- Cataclysm/Assets/Scripts/MainMenuCamera.cs
+++ Cataclysm/Assets/Scripts/MainMenuCamera.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class MainMenuCamera : MonoBehaviour
 {
-
 	public float speed;
 
 	public float leftExtent;
