a015adbfe92399a4189752bfafb16fb2213b0e9a
SavourySnaX <savoury.snax@googlemail.com>
2017-11-08 12:31:12 +0000

    Level 17

--- Cataclysm/Assets/Scripts/ScoreBoard.cs
+++ Cataclysm/Assets/Scripts/ScoreBoard.cs
@@ -102,7 +102,7 @@ public class ScoreBoard : MonoBehaviour
 
 	public bool LevelAvailable(int lvl)
 	{
-		if (lvl >= -3 && lvl <= 16)
+		if (lvl >= -3 && lvl <= 17)
 			return true;
 		return false;
 	}
