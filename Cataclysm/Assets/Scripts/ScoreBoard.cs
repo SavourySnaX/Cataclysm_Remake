8ddc0438a84aca149e8f529d0c0b475e939302d1
SavourySnaX <savoury.snax@googlemail.com>
2017-11-08 16:23:56 +0000

    Level 19 Added down facing gravity gun Added sparkly sprite Added extra colour prefabs for gravity guns Updated 15 to use new sparklies on gravity gun

--- Cataclysm/Assets/Scripts/ScoreBoard.cs
+++ Cataclysm/Assets/Scripts/ScoreBoard.cs
@@ -102,7 +102,7 @@ public class ScoreBoard : MonoBehaviour
 
 	public bool LevelAvailable(int lvl)
 	{
-		if (lvl >= -3 && lvl <= 17)
+		if (lvl >= -3 && lvl <= 19)
 			return true;
 		return false;
 	}
